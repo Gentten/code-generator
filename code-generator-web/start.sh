ec3464e7da019a3b6aa83bcfdaec420ca21ddad3
gentten <1072291061@qq.com>
2019-09-12 15:52:00 +0800

    修改为部署在docker上

--- code-generator-web/start.sh
+++ code-generator-web/start.sh
@@ -1,1 +1,8 @@
-﻿nohup java -jar code-generator-web-0.0.1-SNAPSHOT.jar &
+﻿#删除容器
+docker rm -f code-generator
+#删除镜像
+#docker rmi -f code-generator
+# 创建镜像
+docker build -t code-generator/code-generator  /opt/code-generator
+#创建并允许容器
+docker run  -p 8888:8888 --name code-generator code-generator/code-generator

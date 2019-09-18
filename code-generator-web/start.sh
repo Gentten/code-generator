e29f90eaebaf9cbdf27fcdb1b7f024d61e0c1a14
gentten <1072291061@qq.com>
2019-09-18 14:56:27 +0800

    修改启动设置以及dockerFile

--- code-generator-web/start.sh
+++ code-generator-web/start.sh
@@ -5,7 +5,7 @@ docker rmi -f code-generator/code-generator
 # 创建镜像
 docker build -t code-generator/code-generator  /opt/code-generator
 # 运行容器
-docker run  -d -p 8888:8888 -u root --name code-generator code-generator/code-generator
+docker run  -d -p 8888:8888  --name code-generator code-generator/code-generator
 # 睡眠 1m  等待启动
 sleep 30s
 # 查看最近10m中的日志 以检查启动情况

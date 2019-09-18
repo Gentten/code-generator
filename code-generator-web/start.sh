10ed75363dff78986bbd97dc235e5ad0cf980eee
gentten <1072291061@qq.com>
2019-09-18 14:39:22 +0800

    每次重新构造删除旧的镜像，避免垃圾镜像积累

--- code-generator-web/start.sh
+++ code-generator-web/start.sh
@@ -1,12 +1,12 @@
 ﻿# 删除容器
 docker rm -f code-generator
 # 删除镜像
-docker rmi -f code-generator
+docker rmi -f code-generator/code-generator
 # 创建镜像
 docker build -t code-generator/code-generator  /opt/code-generator
 # 运行容器
 docker run  -d -p 8888:8888 -u root --name code-generator code-generator/code-generator
 # 睡眠 1m  等待启动
-sleep 1m
+sleep 30s
 # 查看最近10m中的日志 以检查启动情况
 docker logs --since 10m code-generator

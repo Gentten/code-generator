5d4ee0a10d6406695f6b5591bbfbb94ed3ee6e34
gentten <1072291061@qq.com>
2019-10-17 16:33:50 +0800

    query模板询bug，复制错位置

--- code-generator-service/src/main/resources/templates/Query.java
+++ code-generator-service/src/main/resources/templates/Query.java
@@ -65,6 +65,8 @@ public class [(${model.className})]Query extends BaseQuery<[(${model.className})
         if ((sorter = this.getSorter()) != null) {
             queryWrapper.orderBy(true, (order = sorter.getOrder()) != null && order.toLowerCase().contains("asc"), StringUtils.humpConvertLowCase(sorter.getField()));
         }
+        //默认按id降序
+        queryWrapper.orderByDesc("id");
         return queryWrapper;
     }
 
@@ -89,13 +91,6 @@ public class [(${model.className})]Query extends BaseQuery<[(${model.className})
                 res.put(getTableColumnName(field), ReflectionUtils.getFieldValue(search, field.getName()));
             }
         });
-        Sorter sorter;
-        String order;
-        if ((sorter = this.getSorter()) != null) {
-        queryWrapper.orderBy(true, (order = sorter.getOrder()) != null && order.toLowerCase().contains("asc"), StringUtils.humpConvertLowCase(sorter.getField()));
-        }
-        //默认按id降序
-        queryWrapper.orderByDesc("id");
         return res;
     }
 
